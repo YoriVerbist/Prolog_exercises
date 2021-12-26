cc3273f1b7a05bbab26d6ffb7f879f2634c7d483
Yori Verbist <yori.verbist1212@gmail.com>
2021-12-26 10:24:09 +0100

    graph, fibonacci, calc, expressive finished

---
+++ oz2/graph.pl
@@ -0,0 +1,33 @@
+node(a).
+node(b).
+node(c).
+node(d).
+node(e).
+
+edge(a,b).
+edge(b,c).
+edge(b,d).
+edge(c,d).
+
+neighbor(X,Y) :-
+    edge(X,Y);
+    edge(Y,X).
+
+path(X, Y) :-
+    neighbor(X,Y).
+
+path(X, Y) :-
+    neighbor(X, Z),
+    path(Z, Y).
+
+path2(X, Y) :-
+    pathHelper(X, Y, [X]).
+
+pathHelper(X, Y, L) :-
+    neighbor(X, Y),
+    \+ member(Y, L).
+
+pathHelper(X, Y, L) :-
+    neighbor(X, Z),
+    \+ member(Z, L),
+    pathHelper(Z, Y, [Z|L]).
