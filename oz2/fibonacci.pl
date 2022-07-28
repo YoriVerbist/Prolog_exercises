40ea08f3c5ad2a754564f81158fb4a03f6fc28c7
Yori Verbist <yori.verbist1212@gmail.com>
2022-07-28 15:52:14 +0200

    Finished OZ2 graphs

--- oz2/fibonacci.pl
+++
@@ -1,9 +0,0 @@
-fib(1, 0).
-fib(2, 1).
-fib(X, Y) :-
-    X > 2,
-    N1 is X - 1,
-    N2 is X - 2,
-    fib(N1, X1),
-    fib(N2, X2),
-    Y is X1 + X2.
--- oz2/prime_number.pl
+++
@@ -1,3 +0,0 @@
-all_primes(N, []) :-
-    N < 2.
-all_primes(2, [2]).
